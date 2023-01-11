e73aa921b64994547ef79cf26543fe69f26b2971
Las02 <lasse101010@gmail.com>
2023-01-11 20:10:51 +0100

    Update main.R

--- main.R
+++ main.R
@@ -14,38 +14,67 @@ clima <- select(WG,!c("wind_gust","wind_chill","heat_index","precip","precip_rat
 # Remove columns with fixed values
 clima <- select(clima, !c("hail","thunder","tornado"))
 
-#formatting data
+# making seperate column for date and time
 clima[c('date','time')] <- str_split_fixed(clima$date,' ',2)
 
 # Picking the correct datatype for the columns
 clima <- mutate(clima,
-                dir  = factor(dir),
-                cond = factor(cond),
-                fog  = factor(fog),
-                rain = factor(rain),
-                snow = factor(snow),
-                date = factor(date),
-                time = factor(time)
+  across(c(dir, cond, fog, rain, snow, date, time),factor)
 )
 
 ### Finding mode and mean for each date ###
 clima_by_date <- group_by(clima, date)
 
+# Define function to calculate mode
 mode <- function(factors){
-factors %>% 
-  table() %>% 
-  which.max() %>% 
-  as.data.frame() %>% 
-  rownames() %>% 
-  factor()
+  max <- factors %>% 
+    table() %>% 
+    which.max() %>% 
+    as.data.frame() %>% 
+    rownames() %>% 
+    factor()
+  
+  # If it does not have a value set it to None for clarity
+  if (max == ""){
+    return(factor("None"))}
+  else {
+    return(max)
+  }  
 }
 
+# Assign the mode and mean using aggregate functions
 clima_by_date %>% 
   summarise(
     across(c(dir, cond, fog, rain, snow),mode),
     across(c(temp, dew_pt, hum, wind_spd, vis, pressure), ~mean(.,na.rm=T))        
     )
 
+### Read in the energy performance of the building
+# Find all the files in ./data
+data_files <- dir("./data", full.names=T)
+
+# Read them into a single dataframe
+# Gives some warnings, but they are all 
+# related to non-important parts of the data
+# ..hopefully
+energy <- NULL
+for (i in seq_along(data_files)){
+  data <- read.table(data_files[i], sep=";") %>% 
+    select(V1, V2, V4)
+  energy <- bind_rows(energy, data)
+}
+
+# Renaming the dataframe
+energy <- energy %>% 
+  rename(id=V1, time=V2, reading=V4)
+
+## Exclude meters with less than 121 records
+# find the records with 121 records
+id_to_keep <- group_by(energy, id) %>% 
+  summarise(n=n()) %>% 
+  filter(n==121)
+# only keep these
+energy <- filter(energy, id %in% id_to_keep$id)
 
 
 
