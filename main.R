f043a6424a17e43ac9681866849647e8dbe0bbc9
Las02 <lasse101010@gmail.com>
2023-01-11 20:35:47 +0100

    Update main.R

--- main.R
+++ main.R
@@ -8,8 +8,10 @@ library(stringr)
 
 load("./WUndergroundHourly.RData")
 
-# Remove all coloumns without data (only NA columns)
-clima <- select(WG,!c("wind_gust","wind_chill","heat_index","precip","precip_rate","precip_total"))
+# Remove all columns without data (only NA columns)
+clima <- select(WG,!c("wind_gust"  , "wind_chill",
+                      "heat_index" , "precip",
+                      "precip_rate", "precip_total"))
 
 # Remove columns with fixed values
 clima <- select(clima, !c("hail","thunder","tornado"))
@@ -43,7 +45,7 @@ mode <- function(factors){
 }
 
 # Assign the mode and mean using aggregate functions
-clima_by_date %>% 
+clima_mean_mode <- clima_by_date %>% 
   summarise(
     across(c(dir, cond, fog, rain, snow),mode),
     across(c(temp, dew_pt, hum, wind_spd, vis, pressure), ~mean(.,na.rm=T))        
@@ -57,7 +59,7 @@ data_files <- dir("./data", full.names=T)
 # \x00 is set to be ignored  since it for some reason is at the end of each file
 energy <- NULL
 for (i in seq_along(data_files)){
-  data <- read.table(data_files[i], sep=";",skipNul=TRUE) %>% 
+  data <- read.table(data_files[i], sep=";", skipNul=TRUE) %>% 
     select(V1, V2, V4)
   energy <- bind_rows(energy, data)
 }
@@ -74,5 +76,5 @@ id_to_keep <- group_by(energy, id) %>%
 # only keep these
 energy <- filter(energy, id %in% id_to_keep$id)
 
-
-
+# set the correct datatypes
+energy <- mutate(energy, time = factor(time), reading = as.numeric(gsub(",", "", reading)))
