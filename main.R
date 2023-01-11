82bd8437a79df95e6c272f1c2b4ef1d6412d1dac
JohannaMunch <johanna@haraldur.com>
2023-01-11 13:27:41 +0100

    Update main.R

--- main.R
+++ main.R
@@ -1,7 +1,8 @@
 #### Introduction ####
 rm(list=ls())
-
 library(tidyverse)
 library(ggplot2)
 library(car)
 
+#### Loading the data ####
+#files <-dir('path_name/', pattern = "*.txt", full.names = TRUE)
