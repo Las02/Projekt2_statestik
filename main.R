cb35629a4e6790b46a7b7f2aa324b929906850d1
Marie-Kragh <mariek535@gmail.com>
2023-01-11 14:27:37 +0100

    Update main.R

--- main.R
+++ main.R
@@ -5,3 +5,24 @@ library(tidyverse)
 library(ggplot2)
 library(car)
 
+load("./WUndergroundHourly.RData")
+summary(WG)
+
+# Remove all coloumns without data (only NA columns)
+clima <- select(WG,!c("wind_gust","wind_chill","heat_index","precip","precip_rate","precip_total"))
+
+# Remove columns with fixed values
+clima <- select(clima, !c("hail","thunder","tornado"))
+
+#formatting data
+
+
+
+# Picking the correct datatype for the columns
+clima <- mutate(clima,
+                dir  = factor(dir),
+                cond = factor(cond),
+                fog  = factor(fog),
+                rain = factor(rain),
+                snow = factor(snow)
+)
