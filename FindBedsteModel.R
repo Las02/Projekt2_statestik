026afb71af14f887d9b127d2a79013f1fb4d2c6f
JohannaMunch <johanna@haraldur.com>
2023-01-15 15:33:44 +0100

    Update FindBedsteModel.R

--- FindBedsteModel.R
+++ FindBedsteModel.R
@@ -200,39 +200,3 @@ models <- arrange(models, AIC) %>%
 xtable(models)
 
 
-#### Important plots ####
-
-## START/END IMPORTANT
-Dplot <- select(D,!c("date", "consumption"))
-
-str(Dplot)
-## Pairsplot ##
-plot(Dplot, col=Dplot$ID, main = "Pairsplot of data")
-
-## Histogram of consumption ##
-hist(Dplot$ncons, col="orange", border = "brown", main = "Normalized consumpution", xlab="Normalized consumption")
-
-## Scatterplot ##
-#It looks like end/start date has an effect
-ggplot(D,aes(x=date, y=ncons,col=ID)) + 
-  geom_point(size=0.8) + theme(legend.position = "none") +
-  labs(y="Normalized consumption", x= "Date", title ="Date as a function of normalized consumption")
-
-#nconc ~ tempdif
-ggplot(D, aes(x=tempdif, y=ncons, col=ID))+
-  geom_point(size=0.8)+ theme(legend.position = "none")+
-  labs(y="Normalized consumption", x= "Temperature difference", title ="Temperature difference as a function of normalized consumption")
-
-
-ggplot(D, aes(x=tempdif, y=ncons, col=ID))+
-  geom_point(size=0.8)+ theme(legend.position = "none")+geom_smooth(method=lm)+
-  labs(y="Normalized consumption", x= "Temperature difference", title ="Temperature difference as a function of normalized consumption")
-
-
-# But it is explained by the temperature fluxating. Maybe we are modelling the noise
-ggplot(D,aes(x=date, y=tempdif,col="black")) + 
-  geom_point(size=1, col="black") + theme(legend.position = "none") +
-  labs(y="Temperature difference", x= "Date", title ="Date as a function of Temperature difference")
-
-plot(Dplot$ncons ~ Dplot$ID, col=c(1:83), main="Boxplot of normalized consumption as a function of ID", ylab = "Normalized consumption", xlab="ID") 
-
